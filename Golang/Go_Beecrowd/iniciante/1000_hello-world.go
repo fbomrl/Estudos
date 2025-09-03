371d50a2796bd55e78e61c79fb79825b5f4866f0
Fabio Meireles <fbomrl14@gmail.com>
2025-09-02 22:59:49 -0300

    Inclusão atividades básicas do básico do beecrowd

---
+++ Golang/Go_Beecrowd/iniciante/1000_hello-world.go
@@ -0,0 +1,7 @@
+package iniciante
+
+import "fmt"
+
+func helloWorld() {
+	fmt.Println("Hello World!")
+}
---
+++ Golang/Go_Beecrowd/iniciante/1001_extremamente-basico.go
@@ -0,0 +1,14 @@
+package iniciante
+
+import "fmt"
+
+func extremamenteBasico() {
+	var A int
+	var B int
+	fmt.Scan(&A)
+	fmt.Scan(&B)
+
+	X := A + B
+
+	fmt.Println("X =", X)
+}
