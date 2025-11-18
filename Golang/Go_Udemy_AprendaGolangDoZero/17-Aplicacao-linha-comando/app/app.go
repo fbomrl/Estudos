f409c038ab98ee5cf57640102fbe7b16a06ea58e
Fabio Meireles <fbomrl14@gmail.com>
2025-11-18 08:06:15 -0300

    fix: init create line command program

---
+++ Golang/Go_Udemy_AprendaGolangDoZero/17-Aplicacao-linha-comando/app/app.go
@@ -0,0 +1,11 @@
+package app
+
+import "github.com/urfave/cli"
+
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de linha de comando"
+	app.Usage = "Busca IPs e nomes de servidor na internet"
+
+	return app
+}
